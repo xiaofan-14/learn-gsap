41d870b4fb2c527ed8b1b0459855ca55e1a9c329
xiaofan <ningfengzeng@gmail.com>
2025-07-20 14:39:18 +0800

    chore: import assect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import NavBar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
     <>
-      <section className="flex-center h-[100vh]">
-        <div className="text-3xl text-indigo-300">the app.jsx</div>
-      </section>
+     <main>
+      <NavBar />
+      </main> 
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { navLinks } from "../../constants";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="./images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
