4790e57a0741661cd7438a95f2434ccba748081e
xiaofan <ningfengzeng@gmail.com>
2025-07-20 17:00:53 +0800

    feat: add hero section comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
-import NavBar from './components/Navbar'
+import NavBar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
     <>
-     <main>
-      <NavBar />
-      </main> 
+      <main>
+        <NavBar />
+        <Hero />
+      </main>
     </>
   );
 }
